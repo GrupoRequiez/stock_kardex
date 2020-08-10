a26ba41c21a309f1d643be4871dc8b7af215f5fa
Gustavo Flores <gustavofc65@gmail.com>
2020-08-10 13:47:03 -0500

    [UPD] Add module stock_kardex v11 to GitHub GrupoRequiez

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,22 @@
-# -*- coding: utf-8 -*-
+# pylint: disable=manifest-required-author
+# Copyright 2017 Humanytek.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': "Kardex",
+    'summary': """
+    Display the moves of a given product according to the location selected
+    """,
     'author': "Humanytek",
-    'website': "https://github.com/humanytek-team/stock_kardex",
+    'website': "http://www.humanytek.com",
+    'license': "AGPL-3",
     'category': 'Stock',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'stock',
-    ],
+    'version': '11.0.1.0.0',
+    'depends': ['stock'],
     'data': [
         'view/stock_kardex_view.xml',
     ],
+    'demo': [
+    ],
+    'installable': True
 }
